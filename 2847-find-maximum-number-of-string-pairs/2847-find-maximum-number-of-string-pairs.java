1e50e6ac89abb377d092c838798c7497d7e35e18
Priyanka Prasad <impriyanka.2609@gmail.com>
2023-08-21 16:16:57 +0530

    Time: 7 ms (33.64%), Space: 42.9 MB (69.08%) - LeetHub

--- 2847-find-maximum-number-of-string-pairs/2847-find-maximum-number-of-string-pairs.java
+++ 2847-find-maximum-number-of-string-pairs/2847-find-maximum-number-of-string-pairs.java
@@ -1,17 +1,16 @@
 class Solution {
     public int maximumNumberOfStringPairs(String[] words) {
         int count = 0;
-        Map<String, Integer> map = new HashMap<>();
-        for(String s: words){
-            String reverse = new StringBuilder(s).reverse().toString();
-            if(map.containsKey(reverse))
-               map.put(reverse, map.get(reverse)+1);
-            else
-               map.put(s, 0);
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < words.length; i++){
+            for(int j = i + 1; j < words.length; j++){
+                sb.append(words[j]);
+                if(words[i].equals(sb.reverse().toString())){
+                    count++;
+                }
+                sb.delete(0, sb.length());
+            }
         }
-        int ans = 0;
-        for(int value: map.values())
-          ans += value;
-          return ans;
+        return count;
     }
 }
