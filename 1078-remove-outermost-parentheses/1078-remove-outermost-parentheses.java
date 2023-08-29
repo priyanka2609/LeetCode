8e660d71921ac9d8dfebfe762ce80cce67d4121b
Priyanka Prasad <impriyanka.2609@gmail.com>
2023-08-29 15:05:38 +0530

    Time: 8 ms (52%), Space: 40.9 MB (88.05%) - LeetHub

---
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        Stack<Character> st = new Stack<>();
+        StringBuilder sb = new StringBuilder("");
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                if(st.size()>0){
+                    sb.append(s.charAt(i));
+                }
+                st.push(s.charAt(i));
+            }else{
+                st.pop();
+                if(st.size()>0){
+                    sb.append(s.charAt(i));
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
