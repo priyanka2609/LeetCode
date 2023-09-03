8af36bf208443e7db0e7405e0a40c4215162d537
Priyanka Prasad <impriyanka.2609@gmail.com>
2023-09-03 21:28:40 +0530

    Time: 0 ms (100%), Space: 39.9 MB (46.78%) - LeetHub

---
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int findTheWinner(int n, int k) {
+        return jos(n, k)+1;
+    }
+    public int jos(int n, int k){
+        if(n == 1) return 0;
+        return (jos(n-1, k) + k) % n;
+    }
+}
